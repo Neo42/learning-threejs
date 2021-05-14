0fb9429ed998f891437e0056e20f01d6d22c2b29
Jiang Hao <giovannilance94@gmail.com>
2021-05-14 22:20:04 +0800

    feat: :tada: first commit

---
+++ src/script.js
@@ -0,0 +1,38 @@
+import './style.css'
+import {
+  Scene,
+  Mesh,
+  BoxGeometry,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  AxesHelper,
+  WebGLRenderer,
+} from 'three'
+
+const canvas = document.querySelector('.webgl')
+
+// scene
+const scene = new Scene()
+const helper = new AxesHelper()
+scene.add(helper)
+
+// box
+const mesh = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({color: 0xff0000})
+)
+
+scene.add(mesh)
+
+// camera
+const sizes = {
+  width: 800,
+  height: 600,
+}
+const camera = new PerspectiveCamera(55, sizes.width / sizes.height)
+scene.add(camera)
+
+// renderer
+const renderer = new WebGLRenderer({canvas})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
